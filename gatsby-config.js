f5a2a0384a85f827699d8261e630ef0ed1fbabc0
tmhuyett <toddhuyett@gmail.com>
2021-01-17 21:08:20 -0500

    'dfdfd'

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `OneShopper`,
+        name: `eMsShop`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#4DB58E`,
